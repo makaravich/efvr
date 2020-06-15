9e19b783f3fc0dadf984b7faf997ea399c6073cd
makaravich <d.makarski@gmail.com>
2020-06-15 14:05:57 +0300

    Update efvr-section.php

--- widgets/efvr-section.php
+++ widgets/efvr-section.php
@@ -78,7 +78,7 @@ add_action('elementor/element/after_section_end', function ($element, $section_i
                 'label' => '',
                 'type' => \Elementor\Controls_Manager::RAW_HTML,
                 'default' => '',
-                'raw' => 'jQuery(\'.elementor-repeater-row-tool.elementor-repeater-tool-remove\').each(function(){jQuery(this).click(console.log(\'!!!\');});',
+                'raw' => fvr_get_admin_js(),
             ]
         );
 
@@ -86,3 +86,39 @@ add_action('elementor/element/after_section_end', function ($element, $section_i
         $element->end_controls_section();
     }
 }, 10, 3);
+
+function fvr_get_admin_js()
+{
+    ob_start();
+    ?>
+    <button id="fvr-clear-all">Clear All</button>
+    <button id="fvr-add-fake">Add Fake</button>
+    <script>
+        //jQuery('.elementor-repeater-row-tool.elementor-repeater-tool-remove').click();
+        jQuery('#fvr-clear-all').click(function () {
+            jQuery('.elementor-repeater-row-tool.elementor-repeater-tool-remove').each(function () {
+                jQuery(this).click();
+                console.log('!!!');
+            });
+        });
+
+
+        // We add an element to the repeater by simulating a click on the add button
+        jQuery('#fvr-add-fake').click(function () {
+            // We grab the latest item of the Repeater field (the one we just triggered the creation of
+            var jQuery_scope_repeater_row = jQuery('.elementor-control-fvr_list .elementor-repeater-fields:last-child');
+
+            // We set the value of the input field
+            jQuery('.elementor-control .elementor-control-fvr_controller_name .elementor-control-type-text input[data-setting="fvr_controller_name"]', jQuery_scope_repeater_row).val('MY_FIELD_VALUE');
+
+            // We trigger the input event to ask Elementor to update the widget preview
+            jQuery('.elementor-control .elementor-control-fvr_controller_name .elementor-control-type-text input[data-setting="fvr_controller_name"]', jQuery_scope_repeater_row).trigger('input');
+
+        });
+
+
+    </script>
+    <?php
+    return ob_get_clean();
+
+}
